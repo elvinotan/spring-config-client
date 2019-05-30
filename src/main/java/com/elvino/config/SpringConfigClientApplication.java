09f8aff2abfd871d156c8d91308b79345bf77ca8
elvino <elvino_tan@yahoo.com>
2019-05-30 14:25:54 +0700

    Ubah nama file

--- src/main/java/com/elvino/config/SpringConfigClientApplication.java
+++ src/main/java/com/elvino/config/SpringConfigClientApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ClientApplication {
+public class SpringConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
+		SpringApplication.run(SpringConfigClientApplication.class, args);
 	}
 
 }
